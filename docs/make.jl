3bb15d772894e7996c6d5c2d8a95092ca8db535e
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:24:12 +0000

    Snapshot of branch master taken on 2019-04-07T15:24:11.571-04:00 from "https://github.com/UnofficialJuliaMirror/Bootstrap.jl-e28b5b4c-05e8-5b66-bc03-6f0c0a0a06e0"

---
+++ docs/make.jl
@@ -0,0 +1,27 @@
+using Documenter
+using Bootstrap
+
+makedocs(
+    modules = [Bootstrap],
+    format = :html,
+    sitename = "Bootstrap.jl",
+    doctest = true,
+    checkdocs = :exports,
+    linkcheck = true,
+    html_disable_git = true, # disable source and edit links to github
+    html_canonical = "https://juliangehring.github.io/Bootstrap.jl/stable/",
+    pages = [
+        "Home" => "index.md",
+        "Library" => "library.md",
+        "Changelog" => "NEWS.md",
+        "License" => "LICENSE.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/juliangehring/Bootstrap.jl.git",
+    julia = "1.0",
+    target = "build",
+    deps   = nothing,
+    make   = nothing
+)
---
+++ status.md
@@ -0,0 +1,7 @@
+# Package Status
+
+[![DOI](https://zenodo.org/badge/25918430.svg)](https://zenodo.org/badge/latestdoi/25918430)
+
+[![Linux/Mac Build Status](https://travis-ci.org/juliangehring/Bootstrap.jl.svg?branch=master)](https://travis-ci.org/juliangehring/Bootstrap.jl)
+[![Windows Build status](https://ci.appveyor.com/api/projects/status/l8qifrqaj3gxloxg/branch/master?svg=true)](https://ci.appveyor.com/project/juliangehring/bootstrap-jl-433wi/branch/master)
+[![Test Coverage Status](https://codecov.io/gh/juliangehring/Bootstrap.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/juliangehring/Bootstrap.jl)
